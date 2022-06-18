1d6ba7506a1dea95107ad6be146d17b09ae2a6d2
Hashem Alkeshawi <hashemalkeshawi@gmail.com>
2022-06-18 20:24:54 +0300

    Image-widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,32 +8,43 @@ class App1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello world!!"),
-        ),
-        body: Container(
-          width: 300,
-          height: 300,
-          padding: const EdgeInsets.all(8),
-          margin: const EdgeInsets.all(6),
-          decoration: const BoxDecoration(
-              color: Colors.redAccent,
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage("recourses/flutter.png"),
-              )),
-          child: const Text(
-            "Hello world!",
-            style:
-                TextStyle(color: Color.fromARGB(255, 54, 244, 117), shadows: [
-              Shadow(
-                  color: Color.fromARGB(255, 62, 75, 202),
-                  offset: Offset(20, 20)),
-            ]),
-          ),
-        ),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("Hello world!"),
       ),
-    );
+      // ========== IMAGE WIDGET ==============////
+      body: Container(
+        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+        child: (Image.asset(
+          "recourses/flutter.png",
+          width: 400,
+          height: 200,
+          fit: BoxFit.cover,
+        )),
+      ),
+    )
+
+        // ====== CONTAINERS ===========////
+
+        // Container(
+        //   width: 200,
+        //   height: 100,
+        //   margin: const EdgeInsets.only(top: 100, left: 80),
+        //   alignment: Alignment.center,
+        //   decoration: const BoxDecoration(color: Colors.redAccent, boxShadow: [
+        //     BoxShadow(
+        //         color: Colors.black,
+        //         blurRadius: 9,
+        //         spreadRadius: 0.5,
+        //         offset: Offset(5, 5))
+        //   ]),
+        //   child: const Text(
+        //     "Hello world!",
+        //     // textAlign: TextAlign.center,
+        //     style: TextStyle(
+        //         color: Color.fromARGB(255, 54, 244, 117), shadows: []),
+        //   ),
+        // ),
+        );
   }
 }
