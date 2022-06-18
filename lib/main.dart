49a28cd1f875ccce9b07df71ef6d93f7dc9e26e1
Hashem Alkeshawi <hashemalkeshawi@gmail.com>
2022-06-18 19:16:24 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class App1 extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Hello world!"),
+          title: Text("Hello world!!"),
         ),
         body: Container(
           width: 300,
