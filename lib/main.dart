8fea7bf4f6f381a3654634685a11ec096513de0e
Hashem Alkeshawi <hashemalkeshawi@gmail.com>
2022-06-19 11:18:49 +0300

    stackWidgit

--- lib/main.dart
+++ lib/main.dart
@@ -9,113 +9,180 @@ class App1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              title: Text("Hello world!"),
+      appBar: AppBar(
+        title: Text("Hello world!"),
+      ),
+      // ====================StackWdiget===============/////
+
+      body: Container(
+        width: 400,
+        height: 400,
+        color: Colors.black,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              width: 350,
+              height: 350,
+              color: Colors.red,
+              child: Text("First"),
             ),
+            Container(
+              // margin: EdgeInsets.all(35),
+              width: 300,
+              height: 300,
+              color: Colors.green,
+              child: Text("second"),
+            ),
+            Container(
+              // margin: EdgeInsets.all(70),
+              width: 200,
+              height: 200,
+              color: Colors.blue,
+              child: Text("third"),
+            ),
+          ],
+        ),
+      ),
+    ));
 
-            // ========== CARD WIDGET with a container in a column()==============////
-            body: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-                  child: (Image.asset(
-                    "recourses/flutter.png",
-                    width: 400,
-                    height: 200,
-                    fit: BoxFit.cover,
-                  )),
-                ),
-                Card(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-                  color: Color.fromARGB(255, 240, 120, 120),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    side: BorderSide(color: Color(0xff328943), width: 5),
-                  ),
-                  elevation: 12,
-                  shadowColor: Colors.blueGrey,
-                  child: Text(
-                    "First Card",
-                    style: TextStyle(fontSize: 40),
-                  ),
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Card(
-                      margin: EdgeInsets.symmetric(vertical: 10),
-                      color: Color.fromARGB(255, 240, 120, 120),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                        side: BorderSide(color: Color(0xff328943), width: 5),
-                      ),
-                      elevation: 12,
-                      shadowColor: Colors.blueGrey,
-                      child: const Text(
-                        "Card1",
-                        style: TextStyle(fontSize: 26),
-                      ),
-                    ),
-                    Card(
-                      margin: EdgeInsets.symmetric(vertical: 10),
-                      color: Color.fromARGB(255, 240, 120, 120),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                        side: BorderSide(color: Color(0xff328943), width: 5),
-                      ),
-                      elevation: 12,
-                      shadowColor: Colors.blueGrey,
-                      child: const Text(
-                        "Card2",
-                        style: TextStyle(fontSize: 26),
-                      ),
-                    ),
-                    Text(
-                      "text",
-                      style: TextStyle(fontSize: 28),
-                    ),
-                  ],
-                ),
-              ],
-            ))
+    // ====================StackWdiget===============/////
+    //   body: Container(
+    //     width: 400,
+    //     height: 400,
+    //     color: Colors.black,
+    //     child: Stack(
+    //       alignment: Alignment.center,
+    //       children: [
+    //         Container(
+    //           width: 350,
+    //           height: 350,
+    //           color: Colors.red,
+    //           child: Text("First"),
+    //         ),
+    //         Container(
+    //           // margin: EdgeInsets.all(35),
+    //           width: 300,
+    //           height: 300,
+    //           color: Colors.green,
+    //           child: Text("second"),
+    //         ),
+    //         Container(
+    //           // margin: EdgeInsets.all(70),
+    //           width: 200,
+    //           height: 200,
+    //           color: Colors.blue,
+    //           child: Text("third"),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // ));
 
-        // ========== IMAGE WIDGET ==============////
+    // ========== CARD WIDGET with a container in a column() and Raw()==============////
 
-        //     Container(
-        //   margin: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-        //   child: (Image.asset(
-        //     "recourses/flutter.png",
-        //     width: 400,
-        //     height: 200,
-        //     fit: BoxFit.cover,
-        //   )),
-        // ))
+    //  Column(
+    //   mainAxisAlignment: MainAxisAlignment.start,
+    //   crossAxisAlignment: CrossAxisAlignment.stretch,
+    //   children: [
+    //     Container(
+    //       margin: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+    //       child: (Image.asset(
+    //         "recourses/flutter.png",
+    //         width: 400,
+    //         height: 200,
+    //         fit: BoxFit.cover,
+    //       )),
+    //     ),
+    //     Card(
+    //       margin: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+    //       color: Color.fromARGB(255, 240, 120, 120),
+    //       shape: RoundedRectangleBorder(
+    //         borderRadius: BorderRadius.circular(8),
+    //         side: BorderSide(color: Color(0xff328943), width: 5),
+    //       ),
+    //       elevation: 12,
+    //       shadowColor: Colors.blueGrey,
+    //       child: Text(
+    //         "First Card",
+    //         style: TextStyle(fontSize: 40),
+    //       ),
+    //     ),
+    //     Row(
+    //       mainAxisSize: MainAxisSize.max,
+    //       mainAxisAlignment: MainAxisAlignment.spaceAround,
+    //       crossAxisAlignment: CrossAxisAlignment.start,
+    //       children: [
+    //         Card(
+    //           margin: EdgeInsets.symmetric(vertical: 10),
+    //           color: Color.fromARGB(255, 240, 120, 120),
+    //           shape: RoundedRectangleBorder(
+    //             borderRadius: BorderRadius.circular(8),
+    //             side: BorderSide(color: Color(0xff328943), width: 5),
+    //           ),
+    //           elevation: 12,
+    //           shadowColor: Colors.blueGrey,
+    //           child: const Text(
+    //             "Card1",
+    //             style: TextStyle(fontSize: 26),
+    //           ),
+    //         ),
+    //         Card(
+    //           margin: EdgeInsets.symmetric(vertical: 10),
+    //           color: Color.fromARGB(255, 240, 120, 120),
+    //           shape: RoundedRectangleBorder(
+    //             borderRadius: BorderRadius.circular(8),
+    //             side: BorderSide(color: Color(0xff328943), width: 5),
+    //           ),
+    //           elevation: 12,
+    //           shadowColor: Colors.blueGrey,
+    //           child: const Text(
+    //             "Card2",
+    //             style: TextStyle(fontSize: 26),
+    //           ),
+    //         ),
+    //         Text(
+    //           "text",
+    //           style: TextStyle(fontSize: 28),
+    //         ),
+    //       ],
+    //     ),
+    //   ],
+    // ))
 
-        // ====== CONTAINERS ===========////
+    // ========== IMAGE WIDGET ==============////
 
-        // Container(
-        //   width: 200,
-        //   height: 100,
-        //   margin: const EdgeInsets.only(top: 100, left: 80),
-        //   alignment: Alignment.center,
-        //   decoration: const BoxDecoration(color: Colors.redAccent, boxShadow: [
-        //     BoxShadow(
-        //         color: Colors.black,
-        //         blurRadius: 9,
-        //         spreadRadius: 0.5,
-        //         offset: Offset(5, 5))
-        //   ]),
-        //   child: const Text(
-        //     "Hello world!",
-        //     // textAlign: TextAlign.center,
-        //     style: TextStyle(
-        //         color: Color.fromARGB(255, 54, 244, 117), shadows: []),
-        //   ),
-        // ),
-        );
+    //     Container(
+    //   margin: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+    //   child: (Image.asset(
+    //     "recourses/flutter.png",
+    //     width: 400,
+    //     height: 200,
+    //     fit: BoxFit.cover,
+    //   )),
+    // ))
+
+    // ====== CONTAINERS ===========////
+
+    // Container(
+    //   width: 200,
+    //   height: 100,
+    //   margin: const EdgeInsets.only(top: 100, left: 80),
+    //   alignment: Alignment.center,
+    //   decoration: const BoxDecoration(color: Colors.redAccent, boxShadow: [
+    //     BoxShadow(
+    //         color: Colors.black,
+    //         blurRadius: 9,
+    //         spreadRadius: 0.5,
+    //         offset: Offset(5, 5))
+    //   ]),
+    //   child: const Text(
+    //     "Hello world!",
+    //     // textAlign: TextAlign.center,
+    //     style: TextStyle(
+    //         color: Color.fromARGB(255, 54, 244, 117), shadows: []),
+    //   ),
+    // ),
+    // );
   }
 }
