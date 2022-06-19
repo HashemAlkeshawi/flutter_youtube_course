3ff534f3852835ab18e67f3132ec261fd9d645d9
Hashem Alkeshawi <hashemalkeshawi@gmail.com>
2022-06-19 12:42:21 +0300

    EXPANDED in a column and raw

--- lib/main.dart
+++ lib/main.dart
@@ -9,46 +9,114 @@ class App1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text("Hello world!"),
-      ),
-      // ====================StackWdiget===============/////
-
-      body: Container(
-        width: 400,
-        height: 400,
-        color: Colors.black,
-        child: Stack(
-          // alignment: Alignment.center,
-          clipBehavior: Clip.none,
-          children: [
-            Positioned(
-              top: 220,
-              child: Container(
-                width: 200,
-                height: 200,
-                color: Colors.red,
-                child: Text("First"),
-              ),
+            appBar: AppBar(
+              title: Text("Hello world!"),
             ),
-            // Container(
-            //   // margin: EdgeInsets.all(35),
-            //   width: 300,
-            //   height: 300,
-            //   color: Colors.green,
-            //   child: Text("second"),
-            // ),
-            // Container(
-            //   // margin: EdgeInsets.all(70),
-            //   width: 200,
-            //   height: 200,
-            //   color: Colors.blue,
-            //   child: Text("third"),
-            // ),
-          ],
-        ),
-      ),
-    ));
+            body: Text("")));
+
+    //      Container(
+    //       width: double.infinity,
+    //       margin: EdgeInsets.all(10),
+    //       child: Column(
+    //         mainAxisAlignment: MainAxisAlignment.start,
+    //         crossAxisAlignment: CrossAxisAlignment.stretch,
+    //         children: [
+    //           Expanded(
+    //             flex: 2,
+    //             child: Container(
+    //               alignment: Alignment.center,
+    //               child: Text("One"),
+    //               color: Colors.green,
+    //             ),
+    //           ),
+    //           Expanded(
+    //             flex: 3,
+    //             child: Container(
+    //               alignment: Alignment.center,
+    //               child: Text("Two"),
+    //               color: Colors.red,
+    //             ),
+    //           ),
+    //           Expanded(
+    //             flex: 4,
+    //             child: Container(
+    //               alignment: Alignment.center,
+    //               child: Text("Three"),
+    //               color: Colors.yellow,
+    //             ),
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+    //   ),
+    // );
+
+    //=========== Expanded in a raw================////
+
+    //      Container(
+    //       margin: EdgeInsets.all(10),
+    //       child: Row(
+    //         children: [
+    //           Expanded(
+    //             flex: 1,
+    //             child: Container(
+    //                 child: Text("One", textAlign: TextAlign.center),
+    //                 color: Colors.green),
+    //           ),
+    //           Expanded(
+    //             flex: 5,
+    //             child: Container(
+    //                 child: Text("Two", textAlign: TextAlign.center),
+    //                 color: Colors.red),
+    //           ),
+    //           Expanded(
+    //             flex: 1,
+    //             child: Container(
+    //                 child: Text("Three", textAlign: TextAlign.center),
+    //                 color: Colors.yellow),
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+    //   ),
+    // );
+
+    // ====================POSITIONED===============/////
+    // Container(
+    //   width: 400,
+    //   height: 400,
+    //   color: Colors.black,
+    //   child: Stack(
+    //     // alignment: Alignment.center,
+    //     clipBehavior: Clip.none,
+    //     children: [
+    //       Positioned(
+    //         top: 220,
+    //         child: Container(
+    //           width: 200,
+    //           height: 200,
+    //           color: Colors.red,
+    //           child: Text("First"),
+    //         ),
+    //       ),
+    // Container(
+    //   // margin: EdgeInsets.all(35),
+    //   width: 300,
+    //   height: 300,
+    //   color: Colors.green,
+    //   child: Text("second"),
+    // ),
+    // Container(
+    //   // margin: EdgeInsets.all(70),
+    //   width: 200,
+    //   height: 200,
+    //   color: Colors.blue,
+    //   child: Text("third"),
+    // ),
+    //       ],
+    //     ),
+    //   ),
+    // ));
 
     // ====================StackWdiget===============/////
     //   body: Container(
